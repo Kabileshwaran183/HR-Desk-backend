935c5c0b1c349cd1775bd99c5a04cf5b0762d635
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-05-09 23:02:15 +0530

    ll

--- models/JobApplication.js
+++ models/JobApplication.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 
 const JobApplicationSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String
-});
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    yearOfGraduation: { type: Number, required: true },
+    gender: { type: String, required: true },
+    experience: { type: String, required: true },
+    skills: { type: String, required: true },
+    location: { type: String, required: true },
+    pincode: { type: String, required: true },
+    jobTitle: { type: String, required: true },
+    resume: { type: String, required: true },
+    status: { type: String, default: "Pending" }
+}, { timestamps: true });
 
 const JobApplication = mongoose.model("JobApplication", JobApplicationSchema);
 module.exports = JobApplication;
